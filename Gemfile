2df575a3d87dda04b8155c628488ee7dad86e783
Thomas Hobohm <superman3275@gmail.com>
2016-08-13 18:44:40 -0500

    Added email validation.

--- Gemfile
+++ Gemfile
@@ -11,6 +11,8 @@ gem 'puma', '~> 3.0'
 gem 'sass-rails', '~> 5.0'
 gem 'semantic-ui-sass', github: 'doabit/semantic-ui-sass'
 gem 'haml-rails'
+# Email validator
+gem 'validates_email_format_of'
 # Use Uglifier as compressor for JavaScript assets
 gem 'uglifier', '>= 1.3.0'
 # Use CoffeeScript for .coffee assets and views
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 class User < ApplicationRecord
   has_secure_password
   validates_presence_of :email, :first_name
+  validates :email, uniqueness: :true
+  validates :email, email_format: {message: 'does not appear to be a valid email address'}
   validates_length_of :email, minimum: 7, maximum: 60
   validates_length_of :first_name, minimum: 1
   validates_length_of :password, minimum: 6
